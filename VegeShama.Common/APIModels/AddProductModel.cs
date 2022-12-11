061ef189b408a8fc9fa1e06167cb5c2c929730e6
V4nII <V4nII@hotmail.com>
2022-12-11 20:48:31 +0100

    Create API Controllers and DTO models

---
+++ VegeShama.Common/APIModels/AddProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VegeShama.Common.APIModels
+{
+    public class AddProductModel
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ VegeShama.Common/APIModels/UpdateUserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace VegeShama.Common.APIModels
+{
+    public class UpdateUserModel
+    {
+        public? string Name { get; set; }
+        public? string Email { get; set; }
+        public? string Login { get; set; }
+        public? string Password { get; set; }
+        public? string VAT_number { get; set; }
+    }
+}
